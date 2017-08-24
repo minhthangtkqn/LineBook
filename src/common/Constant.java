8449eae1cf2afce33aae0585639dae893a6c79af
Thang Hoang <hoangminhthang21051995@gmail.com>
2017-08-24 10:03:20 +0700

    da~ config dc welcome file

---
+++ src/common/Constant.java
@@ -0,0 +1,10 @@
+package common;
+
+public class Constant {
+	
+	// tables' name
+	public static final String TABLE_SACH = "[tblSach]";
+	public static final String TABLE_RAO_BAN = "[tblRaoBan]";
+	public static final String TABLE_NGUOI_DUNG = "[tblNguoiDung]";
+	
+}
